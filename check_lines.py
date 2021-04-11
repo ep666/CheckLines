0ba284651eda4bebef667515ff4868b86337f558
Alexander Maslov <amas4737@gmail.com>
2021-04-11 13:38:56 +0300

    first commit

---
+++ check_lines.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 13:42:42 2021
+
+@author: Alex
+"""
+
+
+import glob2
+import sys
+
+total = 0
+
+
+# Reading from file 
+def lineNumbers (filesInDir):
+    Counter = 0
+    Content = filesInDir.read() 
+    CoList = Content.split("\n") 
+  
+    for i in CoList: 
+        if i: 
+            Counter += 1
+          
+    print(filesInDir.name, '\t - ', Counter)
+    return Counter
+
+
+usrPath = input("Enter path to directory: ")
+   
+sys.stdout = open("SumOfLines.txt", "w")
+
+    
+for file in glob2.glob(usrPath+"\\**\\*.cs"):
+    currentFile = file
+    currentFile = open(currentFile, encoding ="utf8")
+    strNumbers = lineNumbers(currentFile)
+    total +=strNumbers
+    
+print ("Сумма строк\t", total)
